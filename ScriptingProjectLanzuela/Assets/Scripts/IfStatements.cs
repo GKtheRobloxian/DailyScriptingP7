041921df01f4a1c0776a1bc30243614391b02478
Samuel Lanzuela <amlanzuela@gmail.com>
2022-10-26 14:23:24 -0700

    Added Project
    Added the scripting project to GitHub

---
+++ ScriptingProjectLanzuela/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest ()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("My mouth is burning.");
+        }
+        else if(coffeeTemperature < coldLimitTemperature)
+        {
+            print("I appreciate the cold drink, but I wanted my warm coffee.");
+        }
+        else
+        {
+            print("... I feel like something's off, but I'll let it be this time.");
+        }
+    }
+}
